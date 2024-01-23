8c391817290162a3e85aaa30d95a70d45bb2e9c4
Pravin Junghare <pravinjunghare01@gmail.com>
2024-01-23 11:32:15 +0530

    Added TreadLocal and ExtentReport

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -1,10 +1,14 @@
 package com.qa.opencart.factory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -17,6 +21,8 @@ public class DriverFactory {
 	public Properties prop;
 	public OptionsManger optionManger;
 	public static String highlight;
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();// created threadlocal class object and
+																					// given Webdriver
 
 	/**
 	 * this method is initizating the driver on the basis of browser name
@@ -28,17 +34,22 @@ public class DriverFactory {
 	// public WebDriver initDriver(String browserName) {
 	public WebDriver initDriver(Properties prop) {
 		optionManger = new OptionsManger(prop);
-		highlight=prop.getProperty("highlight").trim();
+		highlight = prop.getProperty("highlight").trim();
 		String browserName = prop.getProperty("browser").toLowerCase().trim();
-		
+
 		System.out.println("browser name is:" + browserName);
 
 		if (browserName.equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver(optionManger.getChromeOptions());
+			// driver = new ChromeDriver(optionManger.getChromeOptions());
+			// here we are creating object of chromeclass giving it to threadlocal by using
+			// set method
+			tlDriver.set(new ChromeDriver(optionManger.getChromeOptions()));
+
 		}
 
 		else if (browserName.equalsIgnoreCase("firefox")) {
-			driver = new FirefoxDriver(optionManger.getFirefoxOptions());
+			// driver = new FirefoxDriver(optionManger.getFirefoxOptions());
+			tlDriver.set(new FirefoxDriver(optionManger.getFirefoxOptions()));
 		}
 
 		else if (browserName.equalsIgnoreCase("edge")) {
@@ -51,11 +62,27 @@ public class DriverFactory {
 
 		WebDriverManager.chromedriver().setup();
 
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
+		// driver.manage().deleteAllCookies();
+		// driver.manage().window().maximize();
 		// driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
-		driver.get(prop.getProperty("url"));
-		return driver;
+		// driver.get(prop.getProperty("url"));
+		// return driver;
+
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+		// driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+		getDriver().get(prop.getProperty("url"));
+		return getDriver();
+
+	}
+
+/*
+ * This method will retun local copy of driver
+ */
+
+	public synchronized static WebDriver getDriver() {
+		return tlDriver.get();
+		// get () is ued to get local copy of the driver
 	}
 
 	/**
@@ -79,5 +106,20 @@ public class DriverFactory {
 
 		return prop;
 	}
-
+	
+	/**
+	 * take screenshot
+	 */
+	public static String getScreenshot() {
+		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+		File destination = new File(path);
+		try {
+			org.openqa.selenium.io.FileHandler.copy(srcFile, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return path;
+	}
+	
 }
